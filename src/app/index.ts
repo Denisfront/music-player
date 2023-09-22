f01a9d5425d8f8594645af98c4a4ac267cdf0758
Denis Sannikov <smaylik911@gmail.com>
2023-09-22 23:33:00 +0700

    feat: base settings

--- src/app/index.ts
+++ src/app/index.ts
@@ -2,6 +2,8 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './providers/router';
 
+import './assets/scss/base.scss';
+
 const app = createApp(App);
 
 app.use(router);
---
+++ src/pages/Player.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HomeView',
+});
+</script>
+
+<style>
+.home {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: url('../app/assets/icons/bg.svg') center center / cover no-repeat;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
